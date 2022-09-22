51d8d2831fc58886409e4e734c2fd0c1fe91de60
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-22 15:45:52 -0400

    feat: add class vector

---
+++ Class/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticoDeVectores.Class
+{
+    class Vector
+    {
+        private int number;
+        private int[] arrNumbers;
+
+        public Vector()
+        {
+            number = 0;
+        }
+
+        public void setData(int value)
+        {
+            number = value;
+            arrNumbers = new int[value.ToString().Length];
+        }
+
+        public string getData()
+        {
+            return number.ToString();
+        }
+    }
+}
